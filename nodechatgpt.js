5b23160bb42b42db55fb3a361a45c0dc0a578e2b
Your Name <sm920149@gmail.com>
2023-06-28 12:58:57 +0530

    commited

--- nodechatgpt.js
+++ nodechatgpt.js
@@ -2,7 +2,7 @@ const { Configuration, OpenAIApi } = require("openai");
 const readlineSync = require("readline-sync");
 require("dotenv").config();
 // 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
-
+//
 (async () => {
   const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY,
